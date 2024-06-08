116466dfd20aa5f90839bd556851484e4e4e7b39
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-08 03:12:00 -0700

    Update tests.py

--- qs2csv/tests/tests.py
+++ qs2csv/tests/tests.py
@@ -8,7 +8,10 @@ from ..src.qs2csv import qs_to_csv, qs_to_csv_pd
 
 
 class AllFunctionsTest(TestCase):
+    """Tests all package functions"""
+
     def setUp(self):
+        """Sets up test data"""
         self.fkm = ForeignKeyModel.objects.create()
         self.afm = TestModel.objects.create(
             foreign_key=self.fkm,
@@ -18,6 +21,11 @@ class AllFunctionsTest(TestCase):
         self.afm.save()
         self.qs = TestModel.objects.all()
         self.fields = TestModel._meta.local_fields
+
+    def test_model_str(self):
+        """Tests model __str__ accuracy."""
+        self.assertEqual(self.fkm.pk, "FK Model #{self.pk}")
+        self.assertEqual(self.afm.pk, "AF Model #{self.pk}")
 
     def test_qs_to_csv(self):
         """Tests a standard export with default parameters.
@@ -39,19 +47,9 @@ class AllFunctionsTest(TestCase):
         header_row = body_rows.pop(0)
         self.assertFalse(body_rows)
         self.assertIn("1 day, 0:00:00", header_row)
-        print(header_row)
-        # self.assertEqual(header_row, ",".join(self.fields))
 
     def test_qs_to_csv_pd(self):
-        """Tests a standard ``pandas`` export with default parameters.
-
-        filename = "export.csv"
-        only = []
-        defer = []
-        header = False
-        verbose = True
-        values = False
-        """
+        """Tests a standard pandas export with default parameters."""
         response = qs_to_csv_pd(self.qs)
         self.assertEqual(response.status_code, 200)
         self.assertIn("Content-Type", response.headers)
@@ -64,3 +62,93 @@ class AllFunctionsTest(TestCase):
         self.assertIn("1 days", header_row)
 
         # self.assertEqual(header_row, ",".join(self.fields))
+
+    def test_error_handler(self):
+        """Tests error_handler() when ``values = True``."""
+        self.assertRaises(TypeError, qs_to_csv(self.qs))
+        self.assertRaises(TypeError, qs_to_csv_pd(self.qs))
+
+    def test_filename_format(self):
+        """Tests file formatting errors."""
+        f1 = ("a") * 252
+        self.assertRaises(ValueError, qs_to_csv(self.qs, filename=f1))
+        self.assertRaises(ValueError, qs_to_csv_pd(self.qs, filename=f1))
+        f2 = "1<2.csv"
+        self.assertRaises(ValueError, qs_to_csv(self.qs, filename=f2))
+        self.assertRaises(ValueError, qs_to_csv_pd(self.qs, filename=f2))
+        f3 = "file."
+        self.assertRaises(ValueError, qs_to_csv(self.qs, filename=f3))
+        self.assertRaises(ValueError, qs_to_csv_pd(self.qs, filename=f3))
+
+        response = qs_to_csv(self.qs, filename="x", header=True)
+        self.assertIn(".csv", response.headers["Content-Disposition"])
+        content = response.content.decode("utf-8")
+        cvs_reader = reader(StringIO(content))
+        body_rows = list(cvs_reader)
+        header_row = body_rows.pop(0)
+        self.assertIn("Big Auto", header_row)
+
+    def test_evaluated_warning(self):
+        """Tests pre-evaluated QuerySet warnings."""
+        list(self.qs)
+        self.assertWarns(ResourceWarning, qs_to_csv(self.qs))
+        self.assertWarns(ResourceWarning, qs_to_csv_pd(self.qs))
+
+    def test_only_param(self):
+        """Tests a standard export with only and defer parameters."""
+        only = [
+            "char_field",
+            "boolean_field",
+            "foreign_key",
+            "many_to_many_field",
+            "text_field",
+        ]
+        defer = ["text_field"]
+        response = qs_to_csv(
+            self.qs.values(),
+            only=only,
+            defer=defer,
+            header=True,
+            verbose=False,
+        )
+        content = response.content.decode("utf-8")
+        cvs_reader = reader(StringIO(content))
+        body_rows = list(cvs_reader)
+        header_row = body_rows.pop(0)
+        self.assertEqual(len(header_row), 3)
+        self.assertEqual(len(body_rows), 1)
+
+    def test_only_param_pd(self):
+        """Tests a pandas export with only and verbose parameters."""
+        only = [
+            "char_field",
+            "boolean_field",
+            "foreign_key",
+            "many_to_many_field",
+            "text_field",
+        ]
+        response = qs_to_csv_pd(self.qs.values_list(), only=only, header=True)
+        content = response.content.decode("utf-8")
+        cvs_reader = reader(StringIO(content))
+        body_rows = list(cvs_reader)
+        header_row = body_rows.pop(0)
+        self.assertIn("Text", header_row)
+        self.assertEqual(len(body_rows[0]), 1)
+
+    def test_defer_param(self):
+        """Tests a pandas export with only and verbose parameters."""
+        defer = ["decimal_field", "generic_ip_field"]
+        response = qs_to_csv(self.qs.values_list(), defer=defer)
+        content = response.content.decode("utf-8")
+        cvs_reader = reader(StringIO(content))
+        body_rows = list(cvs_reader)
+        self.assertEqual(len(body_rows[-1]), 14)
+
+    def test_defer_param_pd(self):
+        """Tests a pandas export with defer parameter."""
+        defer = ["duration_field", "many_to_many_field", "many_to_many_field"]
+        response = qs_to_csv(self.qs.values(), defer=defer)
+        content = response.content.decode("utf-8")
+        cvs_reader = reader(StringIO(content))
+        body_rows = list(cvs_reader)
+        self.assertEqual(len(body_rows[0]), 14)
