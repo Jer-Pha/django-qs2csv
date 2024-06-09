a5e1297320d2e9c06032ccfbb12b9e559e6f2f52
Jer-Pha <131234246+Jer-Pha@users.noreply.github.com>
2024-06-09 00:12:51 -0700

    Update apps.py

--- qs2csv/apps.py
+++ qs2csv/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class UserprofilesConfig(AppConfig):
+class qs2csvConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "qs2csv"
